0914cc7ea119a1cb0d94e663229783d3160bb943
caalbert <caalbert@student.42sp.org.br>
2022-07-22 22:51:10 +0200

    feat: implement argc argv

--- main.c
+++ main.c
@@ -6,25 +6,56 @@
 /*   By: caalbert <caalbert@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 15:53:01 by caalbert          #+#    #+#             */
-/*   Updated: 2022/07/15 16:23:34 by caalbert         ###   ########.fr       */
+/*   Updated: 2022/07/22 21:41:58 by caalbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <unistd.h>
+// #include "includes/test.h"
 
+/*
+Flags:
+	original -> to compare with printf
+
+*/
+
+int	write_str(const char *string, int length)
+{
+	write(STDOUT_FILENO, string, length);
+	return (length);
+}
 
 int	main(int argc, char *argv[])
 {
 	char *arg_error;
 	char *arg_error2;
+	char *flag;
 
 	arg_error = "You must to input at lest one parameter.";
 	arg_error2 = "You must declare at least one value as a parameter for the software";
-	if (argc <= 1)
-		return (1);
-	// if (argv > 1)
-		// return (1);
+	flag = argv[1];
+	if (argc >= 1)
+	{
+
+		printf("entrou %s\n", argv[1]);//
+		write_str(argv[1], sizeof(argv[1]));
+		// if (flag == "original")
+		// {
+			printf("Return argC: %d\n", argc);
+			printf ("Characters: %c %c \n", 'a', 65);
+			printf ("Decimals: %d %ld\n", 1977, 650000L);
+			printf ("Preceding with blanks: %10d \n", 1977);
+			printf ("Preceding with zeros: %010d \n", 1977);
+			printf ("Some different radices: %d %x %o %#x %#o \n", 100, 100, 100, 100, 100);
+			printf ("floats: %4.2f %+.0e %E \n", 3.1416, 3.1416, 3.1416);
+			printf ("Width trick: %*d \n", 5, 10);
+			printf ("%s \n", "A string");
+		// }
+	}
 	else
-		return (0);
+		printf("return: %s\n", arg_error2);
 	return (0);
 
 }
+
